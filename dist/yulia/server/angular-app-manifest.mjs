c44a9c98e23242723d5540da411330dffb95b0c3
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2025-04-20 17:12:05 +0300

    mail 4

--- dist/yulia/server/angular-app-manifest.mjs
+++ dist/yulia/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 976, hash: 'f71865de0609b6b701eaa4ac136d76f5145dde41b6dbe3c0d47d0ba119985158', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1002, hash: '7ee1a9624d4b464dc47b3ca2a3b23eab53ff4ad5cad5e557c689a6e271a84e35', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 22007, hash: '19f6e703864ecfbd8103b510c0a112ddb00c55868d75c17a49649e3569e2170c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 4765, hash: '99189a5bf95d83d4982eaef56e60e4f166a4a9ead9b76b510f7c187b37ef91b8', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 976, hash: '5dac6abed55530a8125b26b661837d3dc54509d911b5b4e259395a9615ba107a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1002, hash: '68f75aee14e019278be0dcc272fc3a55ee7b760f7cb9b5f9a8459c763c16fa53', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 22007, hash: '9fd32b37c668c8d9f155cdb677a9fde0e1c7bcf0a18ce3372768c8d8e527625d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 4838, hash: '377e3fdefc6346c269bf8609077634f41cc341024500e3c1388cf595f93e5fc0', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
     'styles-NAF7U7RL.css': {size: 3288, hash: 'tObkM63y1Es', text: () => import('./assets-chunks/styles-NAF7U7RL_css.mjs').then(m => m.default)}
   },
 };
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -10,6 +10,10 @@ import {FormBuilder, ReactiveFormsModule, FormGroup, Validators} from '@angular/
 export class FormComponent {
   private formBuilder = inject(FormBuilder);
   private http = inject(HttpClient);
+  sendMessage:boolean=false;
+  errorMsg:boolean=false;
+  errorText:string='';
+  isSending = false;
 
   form:FormGroup = this.formBuilder.group({
     name:['',Validators.required, Validators.minLength(2)],
@@ -19,13 +23,24 @@ export class FormComponent {
 
   onSubmit(el:HTMLElement){
     if (this.form.invalid) return;
-    el.click();
+    
     console.log(this.form);
-    const formData = this.form.value
+    const formData = this.form.value;
+    console.log(formData, 'formData');
+    this.isSending = true;
     this.http.post('/.netlify/functions/send-email', formData).subscribe({
-      next: () => alert('Лист надіслано!'),
-      error: (err) => alert('Помилка при надсиланні: ' + err.message)
+      next: () => {this.sendMessage = true, el.click(), this.isSending = false},
+      error: (err) => {this.errorText = err.message, this.errorMsg=true, this.isSending = false}
     });
     
+  }
+
+  closeSuccesMsg(){
+    this.sendMessage = false
+  };
+
+  closeErrorMsg(){
+    this.errorMsg = false;
+    this.errorText = ''
   }
 }
