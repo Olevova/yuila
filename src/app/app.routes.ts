a3c17e810522794021cc72dd17375e8182be5b18
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2025-04-20 08:46:31 +0300

    form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
+import { FormComponent } from './components/form/form.component';
 
 export const routes: Routes = [{
     path:'',
     component: LandingComponent
-}];
+},
+{
+    path:'contact',
+    component:FormComponent
+}
+];
