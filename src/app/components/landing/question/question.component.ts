96fbf12458e2fc1a027ffe72d0e40885c46ba243
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2025-04-29 08:30:35 +0300

    some fix

--- src/app/components/landing/question/question.component.ts
+++ src/app/components/landing/question/question.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SvgComponent } from '../../common/svg/svg.component';
 
 @Component({
   selector: 'app-question',
-  imports: [RouterModule ],
+  imports: [RouterModule, SvgComponent ],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
