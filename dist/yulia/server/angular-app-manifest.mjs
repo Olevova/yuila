5ae66cba2cd209b53916b2cd4adf56fdca2bd7ec
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2025-05-20 21:46:43 +0300

    redirect2

--- dist/yulia/server/angular-app-manifest.mjs
+++ dist/yulia/server/angular-app-manifest.mjs
@@ -15,15 +15,21 @@ export default {
   },
   {
     "renderMode": 2,
+    "route": "/404"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/404",
     "route": "/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 25240, hash: '45ca463b6984f1740fb11f2c07be226f1cfb121479d744607cac20b5774a94df', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 25024, hash: '4c69f60d8adb3d3593217e32a512ff25c62fe9c50b009ac9b534149854da7d8c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 30497, hash: 'ecff3ef88b99e3a0c6f4748ac9e807196e8ccfc788c16d750c25fe0753c67cb2', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 57314, hash: '2b403dbacde084e710967030bc93d1e26a7c90528fcd1b4eaba729b69de0ac2e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 25240, hash: 'd95dbad79d9de93fd0e4bb002a1b1c085dfd6c470e60dc4828e86bd11a9eef12', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 25024, hash: '0a902e88107689678e1637998ae81a7ba1feea48e49a33f74fd55a23f3566f5f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 30497, hash: 'bed594ea1ec633a04237e76e94f5f4e75c057597c9ac81f02095a019ef41effb', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 57314, hash: '73f41a9cb9506a1cd8325863b53823cbce592a3b17eba87bfd8a2943842558db', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    '404/index.html': {size: 27403, hash: '97fcbc8c29069ea403f65d0178f5ed95c35594d41bda64379df8662464e9bb27', text: () => import('./assets-chunks/404_index_html.mjs').then(m => m.default)},
     'styles-UX6A6DT3.css': {size: 3861, hash: 'kT7FUXhp9LI', text: () => import('./assets-chunks/styles-UX6A6DT3_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,21 @@ import { LandingComponent } from './components/landing/landing.component';
 import { FormComponent } from './components/form/form.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
-
-export const routes: Routes = [{
-    path:'',
-    component: LandingComponent
-},
-{
-    path:'contact',
-    component:FormComponent
-},
-{
-    path:'**',
-    component:NotFoundComponent
-}
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'contact',
+    component: FormComponent,
+  },
+  { 
+    path: '404',
+    component: NotFoundComponent 
+  }, // Додайте явний маршрут
+  { 
+    path: '**',
+    redirectTo: '404' 
+  },
 ];
