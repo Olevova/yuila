f8531796a8c3c71dedcea5ca8cb2e0ca937655d7
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2025-08-02 12:12:46 +0300

    add payment

--- dist/yulia/server/angular-app-manifest.mjs
+++ dist/yulia/server/angular-app-manifest.mjs
@@ -29,12 +29,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 25269, hash: 'acd00de59bc931b58440586a9d07e959872d90dbece05841de307d8843bc2c4b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 25053, hash: 'f3a3d3140692c0c84e8e146d5deb2cf891ba301e714a105c101faf0040e3dddb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 30526, hash: '83c2ec220a043b732bd91334f10e5add8037ae0fa1bfee4913606092b9d0502c', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'agreement/index.html': {size: 43711, hash: '0c0fc24dfa6a83ddf45cf4e0e7aea597b4e99f2d7918164f383846ecf29f3bec', text: () => import('./assets-chunks/agreement_index_html.mjs').then(m => m.default)},
-    '404/index.html': {size: 27432, hash: '7c87f86339f72798a61d2278001219a98c451bd747f025581c800ad236d130b5', text: () => import('./assets-chunks/404_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 57955, hash: '13f5b68bd453ecc983f53ca414b02a566cb87ace51f5b7e826a03bd0e355a8fd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 25269, hash: '3fc053e382dd640b68f7ad8c8f7c99449bfa020902e66ba5bd59f81b104ad1b1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 25053, hash: 'c32126d3483623b4060031ed256ee3881951261691eaecb1400100c2a14d7a6f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 57955, hash: '0eb7bca83bf05c9e9863ca34f1ecbd1d329d832e5dc922ba0a28496e127f6920', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 30526, hash: '8ac0bdbd567538a06913f773362aa13a3e4047f292b7bf2d4db842717913bdee', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'agreement/index.html': {size: 43711, hash: '3078004a2f5d47c6aae0304966852c2bce7ce152f2948d9adb6ba0a9ee9b4e33', text: () => import('./assets-chunks/agreement_index_html.mjs').then(m => m.default)},
+    '404/index.html': {size: 27432, hash: '0eb4d78f9377148044b0f9c2adaa3391dce30d65731f03c5242f05fe63cec6b2', text: () => import('./assets-chunks/404_index_html.mjs').then(m => m.default)},
     'styles-UX6A6DT3.css': {size: 3861, hash: 'kT7FUXhp9LI', text: () => import('./assets-chunks/styles-UX6A6DT3_css.mjs').then(m => m.default)}
   },
 };
--- src/app/components/landing/price/price.component.ts
+++ src/app/components/landing/price/price.component.ts
@@ -9,9 +9,12 @@ import { Component } from '@angular/core';
 })
 export class PriceComponent {
 
-  payFunc(){
-    console.log("click");
-    
-    window.location.href = 'https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJhbW91bnQiOiI1IiwiY3VycmVuY3kiOiJVQUgiLCJkZXNjcmlwdGlvbiI6ItCc0ZbQuSDRgtC+0LLQsNGAIiwicHVibGljX2tleSI6InNhbmRib3hfaTk4MTQ0MzA2OTQ2IiwibGFuZ3VhZ2UiOiJ1ayJ9&signature=0ZWmIY3+bSER+AUKDR0jfeCzubQ=';
+  payFunc(tariff: 'base' | 'max' | 'exclusive'){
+   const links = {
+    base: 'https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJhbW91bnQiOiIyMjkwMCIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiLQotCw0YDQuNGEINCR0LDQt9C+0LLQuNC5IiwicHVibGljX2tleSI6ImkxNjUzMjQ4MDYyNyIsImxhbmd1YWdlIjoidWsifQ==&signature=57jv8qvGHpZlzjeAxu6W+ZXbkt8=',
+    max: 'https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJhbW91bnQiOiIzMTUwMCIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiLQotCw0YDQuNGEINCc0LDQutGB0LjQvNCw0LvRjNC90LjQuSIsInB1YmxpY19rZXkiOiJpMTY1MzI0ODA2MjciLCJsYW5ndWFnZSI6InVrIn0=&signature=Gfw4y5BD0jT0ZDICQZQ/wYP+1SM=',
+    exclusive: 'https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJhbW91bnQiOiIzMTUwMCIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiLQotCw0YDQuNGEINCV0LrRgdC60LvRjtC30LjQstC90LjQuSIsInB1YmxpY19rZXkiOiJpMTY1MzI0ODA2MjciLCJsYW5ndWFnZSI6InVrIn0=&signature=06z2/7n4tfc/l/c7R1UFLmQTMbc=',
+  };
+  window.location.href = links[tariff];
   }
 }
